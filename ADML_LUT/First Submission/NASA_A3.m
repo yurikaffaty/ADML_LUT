20a511cf25c5ddb2a21eabbfce741b2816de7555
YK <yuri@kemik.gt>
2023-09-18 09:32:33 -0600

    'Init_mtlb'

---
+++ ADML_LUT/First Submission/NASA_A3.m
@@ -0,0 +1,11 @@
+clear all
+clc
+
+%% First Import
+set_1 = load("train_FD001.txt");
+set_2 = load("train_FD002.txt");
+set_3 = load("train_FD003.txt");
+set_4 = load("train_FD004.txt");
+%% ADML Nasa Turbo Fan A3
+
+% Descriptive Statistics
