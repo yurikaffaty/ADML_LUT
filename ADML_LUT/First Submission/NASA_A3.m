fd46269d6f461e2937582f170840cba9b6d24c9f
passacholamas <145365810+passacholamas@users.noreply.github.com>
2023-09-18 20:55:16 +0300

    NASA_A3.m

--- ADML_LUT/First Submission/NASA_A3.m
+++ ADML_LUT/First Submission/NASA_A3.m
@@ -94,3 +94,95 @@ disp('Missing Values in Dataset 4:');
 disp(['Number of Missing Values: ', num2str(missing_values_dataset4)]);
 disp(['Percentage of Missing Values: ', num2str(100 * missing_values_dataset4 / numel(dataset4)), '%']);
 
+% Visualization Dataset
+% Boxplot for distribution and Outliers
+% First, we use zscore (stadardization) for uniform scale of data
+
+scaled_dataset1 = zscore(dataset1(:,3:26));
+scaled_dataset2 = zscore(dataset2(:,3:26));
+scaled_dataset3 = zscore(dataset3(:,3:26));
+scaled_dataset4 = zscore(dataset4(:,3:26));
+
+figure(1);
+subplot(2,2,1)
+boxplot(scaled_dataset1);
+xlabel('Variables')
+ylabel('Data')
+title('Boxplot for train FD001')
+
+subplot(2,2,2)
+boxplot(scaled_dataset2);
+xlabel('Variables')
+ylabel('Data')
+title('Boxplot for train FD002')
+
+subplot(2,2,3)
+boxplot(scaled_dataset3);
+xlabel('Variables')
+ylabel('Data')
+title('Boxplot for train FD003')
+
+subplot(2,2,4)
+boxplot(scaled_dataset4);
+xlabel('Variables')
+ylabel('Data')
+title('Boxplot for train FD004')
+
+% Using histogram to visualize the distribution of each variable.
+
+figure(2);
+plothistogram(dataset1, 1, 'Histogram for train FD001');
+plothistogram(dataset2, 2, 'Histogram for train FD002');
+plothistogram(dataset3, 3, 'Histogram for train FD003');
+plothistogram(dataset4, 4, 'Histogram for train FD004');
+
+
+% Time series plot for measurements over time
+% Number of sensor measurement (from column 6 to 26)
+no_sensor_measurement = 21; 
+
+figure(3);
+
+for i = 1:no_sensor_measurement
+    subplot(5, 5, i); 
+    plot(dataset1(:,2), dataset1(:, i+5));
+    xlabel('Time, in cycles');
+    ylabel('Sensor Measurement Value');
+    title(['Senser Measurement ' num2str(i)]);
+end
+
+figure(4);
+for i = 1:no_sensor_measurement
+    subplot(5, 5, i);  
+    plot(dataset2(:,2), dataset2(:, i+5));
+    xlabel('Time, in cycles');
+    ylabel('Sensor Measurement Value');
+    title(['Senser Measurement ' num2str(i)]);
+end
+
+figure(5);
+for i = 1:no_sensor_measurement
+    subplot(5, 5, i); 
+    plot(dataset3(:,2), dataset3(:, i+5));
+    xlabel('Time, in cycles');
+    ylabel('Sensor Measurement Value');
+    title(['Senser Measurement ' num2str(i)]);
+end
+
+figure(6);
+for i = 1:no_sensor_measurement
+    subplot(5, 5, i); 
+    plot(dataset4(:,2), dataset4(:, i+5));
+    xlabel('Time, in cycles');
+    ylabel('Sensor Measurement Value');
+    title(['Senser Measurement ' num2str(i)]);
+end
+
+% Function for plot histogram
+function plothistogram(dataset, position, titleName)
+    subplot(2, 2, position)
+    for i = 3:26
+        histogram(dataset(:,i));
+    end
+    title(titleName);
+end
