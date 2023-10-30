c5b0e63145e1ac4876e5331f33ebdba1108547c0
passacholamas <145365810+passacholamas@users.noreply.github.com>
2023-10-30 20:36:03 +0200

    Update week6_submission.m

--- week6 Submission/week6_submission.m
+++ week6 Submission/week6_submission.m
@@ -18,11 +18,11 @@ model(3).RULT = load("RUL_FD003.txt");
 model(4).RULT = load("RUL_FD004.txt");
 
 % Model hyperparameters
-calibration_ratio = 0.6; % We want to calibrate with the healthy state of the process, where the cycles are low. So we take the healthy beginning of the observations
+% We want to calibrate with the healthy state of the process, where the cycles are low. 
+% So we take the healthy beginning of the observations
+calibration_ratio = 0.6; 
 
 %% Calculating RUL : RUL(Observation) = maxtime(unit==currentunit) - time(Observation)
-% NOTE! Thre is no need to add the RUL to the dataset. Also, the number of the unit and the cycle number is not a monitored parameter. Your code is very redundant, so I made it a bit more efficient 
-% We can use the RUL in visualisation purposes
 
 % Calculate RUL for training and test datasets
 for i = 1:4  
@@ -38,26 +38,25 @@ for i = 1:4
     RUL_test_rescaled{i} = rescaled_RUL(length(RUL_train{i})+1:end);
 end
 
+%% Preprocessing : Sort the rows, Split the data, and scale and center the data
 
 figure; 
 
 for i = 1:4  
     
-
     % Sorting the data
     % First we sort the rows, THEN we divide into calibration and validation. We want to calibrate with THE START OF THE PROCESS for each unit.
-    % We want to calibrate with only the early cycles.. that's why we sort
-    % the training dataset.
+    % We want to calibrate with only the early cycles. that's why we sort the training dataset.
     [~, order] = sort(model(i).dataset(:,2)); 
     model(i).dataset = model(i).dataset(order,:);
     RUL_train_rescaled{i} = RUL_train_rescaled{i}(order);
-    % Removing the first two columns
-
+  
+    % Removing the first five columns because the operational setting will not change.
     model(i).dataset(:,  1:5)   = [];
     model(i).testdata(:, 1:5)   = [];
 
     
-    % Data in test doesn't need to be sorted
+    % We splitted the data into calibration data (30%) and validation data lo(70%)
     nobs    = length(model(i).dataset(:,1));
     cal     = fix(nobs*0.3);
     idxCal  = logical([ones(cal, 1); zeros(nobs-cal, 1)]);
@@ -65,7 +64,7 @@ for i = 1:4
     model(i).calibration    = model(i).dataset(idxCal,:);
     model(i).validation     = model(i).dataset(idxVal,:);
 
-    % Center and scale the data before PCA
+    % Center and scale the data before PCA, using zscore
     [model(i).calC, model(i).mu, model(i).std] = zscore(model(i).calibration);
     model(i).valC = normalize(model(i).validation, 'center', model(i).mu, 'scale', model(i).std);
     model(i).valC(isnan(model(i).valC)) = 0;
@@ -82,8 +81,13 @@ for i = 1:4
 
 end
 
+% After we looked at the cummulative explained variance plots, we decided
+% to select principal component 1 for dataset 1, 3 for dataset 2, 2 for
+% dataset 3, and 3 for dataset 4.
 
 decidedPCs = [1, 3, 2, 3];
+
+%% Apply PCA, and compute T2 and SPEx
 
 for i = 1:4
     figure;
@@ -136,12 +140,18 @@ for i = 1:4
     end
 end
 
+%% Plot RUL with gradient color
 
 % Define the base color
 A = [0, 1, 1];
 
 % Find the maximum RUL value across datasets for the caption
 max_RUL = max(cellfun(@max, RUL_train));
+
+% An arbitrary large number assuming a dataset won't have more than 50 influential sensors.
+max_sensors = 26; 
+influential_sensor_T2 = NaN(4, max_sensors);
+influential_sensor_SPEx = NaN(4, max_sensors);
 
 % Plotting the control charts in the desired sequence
 for i = 1:4
@@ -156,7 +166,7 @@ for i = 1:4
     colorCal = kron(A, ones(pointsCal,1)) .* rescale(RUL_train_rescaled{i}(1:pointsCal), 0, 1);
     colorVal = kron(A, ones(pointsVal,1)) .* rescale(RUL_train_rescaled{i}(pointsCal+1:end), 0, 1);
     colorTest = kron(A, ones(pointsTest,1)) .* RUL_test_rescaled{i};
-
+    
     figure;
 
     % SPEx control chart
@@ -170,8 +180,16 @@ for i = 1:4
     ylabel('SPEx Value');
     colorbar;
     annotation('textbox', [0.15,0.7,0.3,0.2], 'String', ['0 = RUL 1 and 1 = RUL ' num2str(max_RUL)], 'EdgeColor', 'none');
-
-    % T^2 control chart
+    
+    % Signaling Incoming Failure within the SPEx subplot. We decided to try with cycle 10.
+    signal_cycle = 10; 
+    % Assuming RUL is rescaled between 0 to 1
+    signal_idx_train = find(RUL_train_rescaled{i}(1:pointsCal) <= signal_cycle/100); 
+    signal_idx_test = find(RUL_test_rescaled{i} <= signal_cycle/100);
+    scatter(signal_idx_train, model(i).SPExCal(signal_idx_train), 25, 'r', 'filled', 'Marker', 's');
+    scatter(pointsCal + pointsVal + signal_idx_test, model(i).SPExTest(signal_idx_test), 25, 'r', 'filled', 'Marker', 's');
+   
+    % T2 control chart
     subplot(2, 1, 2);
     hold on;
     scatter(1:pointsCal, model(i).T2Cal, 15, RUL_train_rescaled{i}(1:pointsCal), 'filled');
@@ -183,8 +201,45 @@ for i = 1:4
     colorbar;
     annotation('textbox', [0.15,0.7,0.3,0.2], 'String', ['0 = RUL 1 and 1 = RUL ' num2str(max_RUL)], 'EdgeColor', 'none');
 
+    % Signaling Incoming Failure within the T2 subplot
+    signal_cycle = 10; % or 20
+    signal_idx_train = find(RUL_train_rescaled{i}(1:pointsCal) <= signal_cycle/100); % Assuming RUL is rescaled between 0 to 1
+    signal_idx_test = find(RUL_test_rescaled{i} <= signal_cycle/100);
+
+    scatter(signal_idx_train, model(i).T2Cal(signal_idx_train), 25, 'r', 'filled', 'Marker', 's');
+    scatter(pointsCal + pointsVal + signal_idx_test, model(i).T2Test(signal_idx_test), 25, 'r', 'filled', 'Marker', 's');
+    
+    % For T2
+    out_of_control_idx_T2 = find(model(i).T2Val > model(i).T2Alert);
+    disp(['Number of out-of-control observations in T^2 for dataset ' num2str(i) ': ' num2str(length(out_of_control_idx_T2))]);
+    counter_T2 = 1;
+    for idx = out_of_control_idx_T2'
+        [~, influential_sensor] = max(abs(model(i).valC(idx, :)));
+        influential_sensor_T2(i, counter_T2) = influential_sensor;
+        counter_T2 = counter_T2 + 1;
+    end
+    
+    % For SPEx
+    out_of_control_idx_SPEx = find(model(i).SPExVal > model(i).SPExAlert);
+    disp(['Number of out-of-control observations in SPEx for dataset ' num2str(i) ': ' num2str(length(out_of_control_idx_SPEx))]);
+
+    counter_SPEx = 1;
+    for idx = out_of_control_idx_SPEx'
+        [~, influential_sensor] = max(abs(model(i).valC(idx, :) - (model(i).valC(idx, :) * model(i).P * model(i).P')));
+        influential_sensor_SPEx(i, counter_SPEx) = influential_sensor;
+        counter_SPEx = counter_SPEx + 1;
+    end
+    
 end
 
+% Display the results for the current dataset
+for i = 1:4
+    unique_T2_sensors = unique(influential_sensor_T2(i, ~isnan(influential_sensor_T2(i, :))));
+    unique_SPEx_sensors = unique(influential_sensor_SPEx(i, ~isnan(influential_sensor_SPEx(i, :))));
+    disp(['For dataset ' num2str(i) ', unique influential sensors in T^2 chart are: ' num2str(unique_T2_sensors)]);
+    disp(['For dataset ' num2str(i) ', unique influential sensors in SPEx chart are: ' num2str(unique_SPEx_sensors)]);
+    disp('------------------------------------------------------');
+end
 
 %% Function
 function RUL_train = cal_RUL_train(dataset)
@@ -225,4 +280,3 @@ reconstructed = score * loadings(:,1:comp)';
 residuals   = bsxfun(@minus, data, reconstructed);
 Qfac        = sum(residuals.^2,2);
 end
-
